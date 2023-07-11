1d3eca97ba2df4876f9791f74bfa2406c5e4048d
felipe.prog.al@gmail.com <felipe.prog.al@gmail.com>
2023-07-10 21:21:15 -0300

    Adicionar arquivos de projeto.

---
+++ Pedra/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Pedra
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox2 = new PictureBox();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            tesoura = new Button();
+            pedra = new Button();
+            papel = new Button();
+            label5 = new Label();
+            label6 = new Label();
+            mostra = new Label();
+            label8 = new Label();
+            label7 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.ErrorImage = null;
+            pictureBox2.Image = Properties.Resources.branco;
+            pictureBox2.Location = new Point(421, 111);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(162, 141);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.ErrorImage = Properties.Resources.branco;
+            pictureBox1.Image = Properties.Resources.branco;
+            pictureBox1.Location = new Point(121, 111);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(162, 141);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(174, 76);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 20);
+            label1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(174, 43);
+            label2.Name = "label2";
+            label2.Size = new Size(41, 20);
+            label2.TabIndex = 4;
+            label2.Text = "Você";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(475, 76);
+            label3.Name = "label3";
+            label3.Size = new Size(0, 20);
+            label3.TabIndex = 5;
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(475, 43);
+            label4.Name = "label4";
+            label4.Size = new Size(32, 20);
+            label4.TabIndex = 6;
+            label4.Text = "Bot";
+            label4.Click += label4_Click;
+            // 
+            // tesoura
+            // 
+            tesoura.Location = new Point(203, 305);
+            tesoura.Name = "tesoura";
+            tesoura.Size = new Size(94, 29);
+            tesoura.TabIndex = 7;
+            tesoura.Text = "Tesoura";
+            tesoura.UseVisualStyleBackColor = true;
+            tesoura.Click += button1_Click;
+            // 
+            // pedra
+            // 
+            pedra.Location = new Point(303, 305);
+            pedra.Name = "pedra";
+            pedra.Size = new Size(94, 29);
+            pedra.TabIndex = 8;
+            pedra.Text = "Pedra";
+            pedra.UseVisualStyleBackColor = true;
+            pedra.Click += pedra_Click;
+            // 
+            // papel
+            // 
+            papel.Location = new Point(403, 305);
+            papel.Name = "papel";
+            papel.Size = new Size(94, 29);
+            papel.TabIndex = 9;
+            papel.Text = "Papel";
+            papel.UseVisualStyleBackColor = true;
+            papel.Click += papel_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(174, 262);
+            label5.Name = "label5";
+            label5.Size = new Size(61, 20);
+            label5.TabIndex = 10;
+            label5.Text = "0 ponto";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(475, 262);
+            label6.Name = "label6";
+            label6.Size = new Size(61, 20);
+            label6.TabIndex = 11;
+            label6.Text = "0 ponto";
+            // 
+            // mostra
+            // 
+            mostra.AutoSize = true;
+            mostra.Location = new Point(325, 169);
+            mostra.Name = "mostra";
+            mostra.Size = new Size(0, 20);
+            mostra.TabIndex = 12;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(316, 64);
+            label8.Name = "label8";
+            label8.Size = new Size(69, 20);
+            label8.TabIndex = 13;
+            label8.Text = "contador";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(339, 93);
+            label7.Name = "label7";
+            label7.Size = new Size(17, 20);
+            label7.TabIndex = 14;
+            label7.Text = "0";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(724, 418);
+            Controls.Add(label7);
+            Controls.Add(label8);
+            Controls.Add(mostra);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(papel);
+            Controls.Add(pedra);
+            Controls.Add(tesoura);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Controls.Add(pictureBox2);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Button tesoura;
+        private Button pedra;
+        private Button papel;
+        private Label label5;
+        private Label label6;
+        private Label mostra;
+        private Label label8;
+        private Label label7;
+    }
+}
---
+++ Pedra/Form1.cs
@@ -0,0 +1,274 @@
+namespace Pedra
+{
+    public partial class Form1 : Form
+    {
+        static int pontos = 0;
+        static int pontosb = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static string bot()
+        {
+            Random rnd = new Random();
+            int alea = rnd.Next(3);
+            string bot;
+            if (alea == 0)
+            {
+                bot = "Pedra";
+            }
+
+            else if (alea == 1)
+            {
+                bot = "Papel";
+
+            }
+
+            else
+            {
+                bot = "Tesoura";
+            }
+
+            return bot;
+        }
+
+
+
+        int jogo(string a, string b)
+        {
+            string j;
+
+
+            if (a == b)
+            {
+                j = "e";
+                return 0;
+            }
+            else if (a == "Tesoura" && b == "Papel")
+            {
+                j = "v";
+                return 1;
+            }
+            else if (a == "Pedra" && b == "Tesoura")
+            {
+                j = "v";
+                return 1;
+            }
+            else if (a == "Papel" && b == "Pedra")
+            {
+                j = "v";
+                return 1;
+            }
+            else
+            {
+                j = "d";
+                return 2;
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile(@"..\..\Debug\net6.0-windows\imagens\tesoura.jpg");
+            label1.Text = tesoura.Text;
+            string escolha = bot();
+
+            try
+            {
+                pictureBox2.Image = Image.FromFile(@"..\..\Debug\net6.0-windows\imagens\" + escolha.ToLower() + ".jpg");
+            }
+            catch
+            {
+                pictureBox2.Image = Image.FromFile(@"..\..\Debug\net6.0-windows\imagens\" + escolha.ToLower() + ".png");
+            }
+
+            label3.Text = escolha;
+            int resul = jogo(label1.Text, escolha);
+            if (resul == 0)
+            {
+                mostra.Text = "Empate";
+            }
+
+            else if (resul == 1)
+            {
+                mostra.Text = "Vitoria";
+                pontos++;
+                string g = label7.Text;
+                int h = Int32.Parse(g);
+                h++;
+                label7.Text = h.ToString();
+
+            }
+
+            else
+            {
+                mostra.Text = "Derrota";
+                pontosb++;
+                string g = label7.Text;
+                int h = Int32.Parse(g);
+                h++;
+                label7.Text = h.ToString();
+            }
+
+
+            if (pontos > 1)
+            {
+                label5.Text = pontos + " Pontos";
+            }
+            else
+            {
+                label5.Text = pontos + " Ponto";
+            }
+
+            if (pontosb > 1)
+            {
+                label6.Text = pontosb + " Pontos";
+            }
+            else
+            {
+                label6.Text = pontosb + " Ponto";
+            }
+        }
+
+        private void pedra_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile(@"..\..\Debug\net6.0-windows\imagens\pedra.png");
+            label1.Text = pedra.Text;
+            string escolha = bot();
+            label3.Text = escolha;
+
+            try
+            {
+                pictureBox2.Image = Image.FromFile(@"..\..\Debug\net6.0-windows\imagens\" + escolha.ToLower() + ".jpg");
+            }
+            catch
+            {
+                pictureBox2.Image = Image.FromFile(@"..\..\Debug\net6.0-windows\imagens\" + escolha.ToLower() + ".png");
+            }
+
+            int resul = jogo(label1.Text, escolha);
+            if (resul == 0)
+            {
+                mostra.Text = "Empate";
+            }
+
+            else if (resul == 1)
+            {
+                mostra.Text = "Vitoria";
+                pontos++;
+                string g = label7.Text;
+                int h = Int32.Parse(g);
+                h++;
+                label7.Text = h.ToString();
+
+            }
+
+            else
+            {
+                mostra.Text = "Derrota";
+                pontosb++;
+                string g = label7.Text;
+                int h = Int32.Parse(g);
+                h++;
+                label7.Text = h.ToString();
+
+            }
+
+
+            if (pontos > 1)
+            {
+                label5.Text = pontos + " Pontos";
+            }
+            else
+            {
+                label5.Text = pontos + " Ponto";
+            }
+
+            if (pontosb > 1)
+            {
+                label6.Text = pontosb + " Pontos";
+            }
+            else
+            {
+                label6.Text = pontosb + " Ponto";
+            }
+        }
+
+        private void papel_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile(@"..\..\Debug\net6.0-windows\imagens\papel.jpg");
+            label1.Text = papel.Text;
+            string escolha = bot();
+
+            try
+            {
+                pictureBox2.Image = Image.FromFile(@"..\..\Debug\net6.0-windows\imagens\" + escolha.ToLower() + ".jpg");
+            }
+            catch
+            {
+                pictureBox2.Image = Image.FromFile(@"..\..\Debug\net6.0-windows\imagens\" + escolha.ToLower() + ".png");
+            }
+
+            label3.Text = escolha;
+            int resul = jogo(label1.Text, escolha);
+            if (resul == 0)
+            {
+                mostra.Text = "Empate";
+            }
+
+            else if (resul == 1)
+            {
+                mostra.Text = "Vitoria";
+                pontos++;
+                string g = label7.Text;
+                int h = Int32.Parse(g);
+                h++;
+                label7.Text = h.ToString();
+
+            }
+
+            else
+            {
+                mostra.Text = "Derrota";
+                pontosb++;
+                string g = label7.Text;
+                int h = Int32.Parse(g);
+                h++;
+                label7.Text = h.ToString();
+
+            }
+
+
+            if (pontos > 1)
+            {
+                label5.Text = pontos + " Pontos";
+            }
+            else
+            {
+                label5.Text = pontos + " Ponto";
+            }
+
+            if (pontosb > 1)
+            {
+                label6.Text = pontosb + " Pontos";
+            }
+            else
+            {
+                label6.Text = pontosb + " Ponto";
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
